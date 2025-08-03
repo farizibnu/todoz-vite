931a06ffef5f82c2e1162a6d68508dfb8fd2c32e
farizibnu <fariz2240@gmail.com>
2025-08-04 00:43:40 +0700

    Add initial backend structure with Maven configuration and Spring Boot setup

---
+++ backend/src/main/java/com/todoz/backend/TodozBackendApplication.java
@@ -0,0 +1,13 @@
+package com.todoz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodozBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodozBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/todoz/backend/TodozBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodozBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
